c7ca5d0fe64bbbc5f1d6f4ada4cb794e46a521a3
Konstantin Vyatkin <tino@vtkn.io>
2019-10-26 23:45:17 -0400

    trim

--- .github/actions-scripts/polyfills-sync.js
+++ .github/actions-scripts/polyfills-sync.js
@@ -1,10 +1,14 @@
 const { readFileSync } = require('fs');
 const path = require('path');
-const assert = require('assert')
+const assert = require('assert');
 
 const readme = readFileSync(path.resolve(__dirname, '../../README.md'), 'utf8');
 
-const polyfillsFromDocs = /^```polyfills\s*\n([^`]+)\n^```/m.exec(readme)[1].split('\n').sort();
+const polyfillsFromDocs = /^```polyfills\s*\n([^`]+)\n^```/m
+  .exec(readme)[1]
+  .split('\n')
+  .map(v => v.trim())
+  .sort();
 // @ts-ignore
 const polyfillsFromSettings = require('../../.eslintrc.json').settings.polyfills.sort();
-assert.deepStrictEqual(polyfillsFromDocs, polyfillsFromSettings)
+assert.deepStrictEqual(polyfillsFromDocs, polyfillsFromSettings);
